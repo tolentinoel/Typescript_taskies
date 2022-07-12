1eeaffbdcc1152ca1f33e97b8eea1cb95776f307
tolentinoel <rckrleyn@gmail.com>
2022-07-12 11:50:12 -0700

    adding useRef

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,28 +1,30 @@
-import React, { useRef } from 'react';
 import './styles.css';
+
+import React, { useRef } from 'react';
 
 interface Props {
     todo: string,
     setToDo: React.Dispatch<React.SetStateAction<string>>;
     handleAdd: (e: React.FormEvent) => void;
 }
-const InputField = ({todo, setToDo, handleAdd}: Props) => {
-   const inputRef = useRef<HTMLInputElement>(null);
 
-  return <form className= "input" onSubmit={(e) => {
-            handleAdd(e);
-            inputRef.current?.blur();
-            }}>
-            <input
-                ref={inputRef}
-                value={todo}
-                onChange={(e) => setToDo(e.target.value)}
-                type='input'
-                placeholder="Enter task"
-                className="inputBox"
-            />
-            <button className="inputSubmit" type="submit">Go</button>
-        </form>
+const InputField = ({todo, setToDo, handleAdd}: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return <form className= "input" onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.focus();
+                }}>
+                <input
+                    ref={inputRef}
+                    value={todo}
+                    onChange={(e) => setToDo(e.target.value)}
+                    type='input'
+                    placeholder="Enter task"
+                    className="inputBox"
+                />
+                <button className="inputSubmit" type="submit">Go</button>
+            </form>
 
 }
 
